ec9dc444816773b2f555198479c8c683ed246376
Moemad-711 <mohammedemad4@gmail.com>
2020-06-12 09:53:36 +0200

    Update ManageUserControllerTest.cs

--- DessertShop/DessertShopUnitTest/DessertShopUnitTest/ManageUserControllerTest.cs
+++ DessertShop/DessertShopUnitTest/DessertShopUnitTest/ManageUserControllerTest.cs
@@ -1,36 +1,57 @@
-﻿using NUnit.Framework;
-using DessertShop;
-using DessertShop.Controllers;
-using DessertShop.Models;
-using Microsoft.AspNetCore.Hosting;
-using Moq;
+﻿using DessertShop.Controllers;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Identity;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DessertShopUnitTest
 {
-    [TestFixture]
     class ManageUserControllerTest
     {
         private Mock<UserManager<IdentityUser>> mockUserManger;
-        private ManageUsersController _ManageUsersController;
+
+        private ManageUsersController manageUsersController;
 
         [SetUp]
         public void Setup()
         {
-            mockUserManger = new Mock<UserManager<IdentityUser>>();
-            _ManageUsersController = new ManageUsersController(mockUserManger.Object);
+            mockUserManger = new Mock<UserManager<IdentityUser>>(
+                    new Mock<IUserStore<IdentityUser>>().Object,
+                    new Mock<IOptions<IdentityOptions>>().Object,
+                    new Mock<IPasswordHasher<IdentityUser>>().Object,
+                    new IUserValidator<IdentityUser>[0],
+                    new IPasswordValidator<IdentityUser>[0],
+                    new Mock<ILookupNormalizer>().Object,
+                    new Mock<IdentityErrorDescriber>().Object,
+                    new Mock<IServiceProvider>().Object,
+                    new Mock<ILogger<UserManager<IdentityUser>>>().Object);
+            manageUsersController = new ManageUsersController(mockUserManger.Object);
         }
         [Test]
         public async Task Index()
         {
             const string expected = "DessertShop.ViewModels.ManageUsersViewModel";
 
-            var result = await _ManageUsersController.Index();
+            var userId = Guid.NewGuid().ToString();
+
+            IdentityUser user = new IdentityUser
+            {
+                Id = userId
+            };
+
+            List<IdentityUser> users = new List<IdentityUser>();
+            users.Add(user);
+            mockUserManger.Setup(um => um.CreateAsync(user, "Moemad@admin123"));
+            mockUserManger.Setup(um => um.AddToRoleAsync(user, "Administrator"));
+            mockUserManger.Setup(um => um.GetUsersInRoleAsync("Administrator")).ReturnsAsync(users);
+
+            var result = await manageUsersController.Index() as ViewResult;
 
             Assert.AreEqual("Index", result.ViewName);
             Assert.AreEqual(expected, result.Model.ToString());
