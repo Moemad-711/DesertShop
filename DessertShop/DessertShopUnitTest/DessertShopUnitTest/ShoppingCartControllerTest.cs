fba16e74b7a894d1acfc6c502153730c60202b33
Moemad-711 <mohammedemad4@gmail.com>
2020-06-12 07:57:45 +0200

    Create ShoppingCartControllerTest.cs

---
+++ DessertShop/DessertShopUnitTest/DessertShopUnitTest/ShoppingCartControllerTest.cs
@@ -0,0 +1,37 @@
+﻿@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using DessertShop;
+using DessertShop.Controllers;
+using DessertShop.Models;
+using Microsoft.AspNetCore.Hosting;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+
+namespace DessertShopUnitTest
+{
+    class ShoppingCartControllerTest
+    {
+        private Mock<IPieRepository> mockPieRepository;
+        private Mock<ICakeRepository> mockCakeRepository;
+        private Mock<IShoppingCartRepository> mockShoppingCartRepository;
+
+
+        private ShoppingCartController shoppingCartController;
+
+        [SetUp]
+        public void Setup()
+        {
+            mockPieRepository = new Mock<IPieRepository>();
+            mockShoppingCartRepository = new Mock<IShoppingCartRepository>();
+            mockCakeRepository = new Mock<ICakeRepository>();
+
+            shoppingCartController = new ShoppingCartController(mockPieRepository.Object, mockCakeRepository.Object, mockShoppingCartRepository.Object);
+        }
+
+
+
+    }
+}
