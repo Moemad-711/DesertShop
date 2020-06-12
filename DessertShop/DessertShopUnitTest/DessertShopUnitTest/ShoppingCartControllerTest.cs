6d985185f8bfb52101667bd1361a6f31dbf3905b
Moemad-711 <mohammedemad4@gmail.com>
2020-06-12 05:53:46 +0200

    Create ShoppingCartControllerTest.cs

---
+++ DessertShop/DessertShopUnitTest/DessertShopUnitTest/ShoppingCartControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using DessertShop;
+using DessertShop.Controllers;
+using DessertShop.Models;
+using Microsoft.AspNetCore.Hosting;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DessertShopUnitTest
+{
+    class ShoppingCartControllerTest
+    {
+
+        private ShoppingCartController shoppingCartController;
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+    }
+}
