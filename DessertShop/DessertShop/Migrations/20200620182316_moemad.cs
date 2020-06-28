e46c068f653e16bdb397abfcc5883c57f67c5d18
Moemad-711 <mohammedemad4@gmail.com>
2020-06-28 16:10:04 +0200

    update

---
+++ DessertShop/DessertShop/Migrations/20200620182316_moemad.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DessertShop.Migrations
+{
+    public partial class moemad : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
